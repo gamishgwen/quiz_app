1ca9ca76830151b71af03c070b040489bfa0a7c9
gamishgwen <connectwithgayathrim@gmail.com>
2024-01-26 20:22:13 +0530

    start screen

--- lib/main.dart
+++ lib/main.dart
@@ -1,70 +1,10 @@
+
+
+
 import 'package:flutter/material.dart';
+import 'package:quiz_app/quiz.dart';
+import 'package:quiz_app/start_screen.dart';
+void main(){
 
-void main() {
-  runApp(const MyApp());
-}
-
-class MyApp extends StatelessWidget {
-  const MyApp({super.key});
-
-  @override
-  Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        colorScheme: ColorScheme.fromSeed(seedColor: Colors.deepPurple),
-        useMaterial3: true,
-      ),
-      home: const MyHomePage(),
-    );
-  }
-}
-
-class MyHomePage extends StatefulWidget {
-  const MyHomePage({super.key});
-
-  @override
-  State<MyHomePage> createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  var counter=0;
-  void incrementCounter(){
-
-    setState(() {
-      counter++;
-    });
-
-}
-
-
-  @override
-  Widget build(BuildContext context) {
-    return Scaffold(
-      appBar: AppBar(
-        backgroundColor: Theme.of(context).colorScheme.inversePrimary,
-        title: Text('title'),
-      ),
-      body: Center(
-        child: Column(
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            const Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$counter',
-              style: Theme.of(context).textTheme.headlineMedium,
-            ),
-          ],
-        ),
-      ),
-      floatingActionButton: FloatingActionButton(
-
-        onPressed: incrementCounter,
-        tooltip: 'Increment',
-        child: const Icon(Icons.add),
-      ),
-    );
-  }
+  runApp(Quiz());
 }
---
+++ lib/start_screen.dart
@@ -0,0 +1,42 @@
+import 'package:flutter/material.dart';
+
+class StartScreen extends StatelessWidget {
+  const StartScreen(this.startQuiz, {super.key});
+
+  final void Function() startQuiz;
+
+  @override
+  Widget build(context) {
+    return Center(
+      child: Column(
+        mainAxisSize: MainAxisSize.min,
+        children: [
+          Opacity(
+            opacity: 0.5,
+            child: Image.asset(
+              'assets/images/quiz-logo.png',
+              width: 300,
+            ),
+          ),
+          const SizedBox(height: 30),
+          const Text(
+            'Learn flutter in fun way',
+            style: TextStyle(
+              color: Colors.purple,
+              fontSize: 24,
+            ),
+          ),
+          const SizedBox(height: 44),
+          OutlinedButton.icon(
+            onPressed: startQuiz,
+            style: OutlinedButton.styleFrom(
+              foregroundColor: Colors.green,
+            ),
+            icon: const Icon(Icons.arrow_right_alt),
+            label: const Text('Start Quiz'),
+          )
+        ],
+      ),
+    );
+  }
+}
